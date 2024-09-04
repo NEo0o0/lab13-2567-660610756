0103544a831b4cc8c34f81e04c429d38da631808
NEo0o0 <nathdanai@gmail.com>
2024-09-04 15:46:47 +0700

    complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Nathdanai Thipdonjun"
+            studentId="660610756"
           />
         </div>
       </body>
